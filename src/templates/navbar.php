e551206311897e38953010d4941de416975dbc33
Helpad <202100542@umh.edu.hn>
2021-05-07 20:59:55 -0600

    Navbar actualizado para que redireccione bien

--- src/templates/navbar.php
+++ src/templates/navbar.php
@@ -5,9 +5,9 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
         <a class="navbar-brand" href="index.php">StockApps</a>
-        <a class="nav-link active" href="../src/features/producto.php"><i class="bi bi-people"></i> Usuarios</a>
-        <a class="nav-link active" href="../src/features/categorias.php"><i class='bi bi-tags'></i> Categorías</a>
-        <a class="nav-link active" href="../src/features/usuarios.php"><i  class='bi bi-person-square'></i> Usuarios</a>
+        <a class="nav-link active" href="../public/productos.php"><i class="bi bi-bag"></i> Productos</a>
+        <a class="nav-link active" href="../public/categorias.php"><i class='bi bi-tags'></i> Categorías</a>
+        <a class="nav-link active" href="../public/usuarios.php"><i  class='bi bi-person-square'></i> Usuarios</a>
         <a class="nav-link active" href="login.php?logout">Salir</a>
       </div>
     </nav>
